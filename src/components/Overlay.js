d4909af796019d1fef4f2930055a52988cbcd9f4
asalexanderlee <asalexanderlee@davidson.edu>
2018-09-08 13:12:51 -0400

    Add overlay circle

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,90 +1,15 @@
 import React, { Component } from "react";
 import "./Overlay.css";
 export default class Overlay extends Component {
+  handleClick() {
+    console.log("works");
+  }
   render() {
     return (
       <React.Fragment>
         <div id="overlayWrapper">
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-          </div>
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-          </div>
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-          </div>
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-          </div>
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-          </div>
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-          </div>
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-          </div>
-
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-          </div>
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-          </div>
-          <div class="overlayColumn">
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
-            <div class="overlayRow" />
+          <div className="circle" onClick={this.handleClick}>
+            69
           </div>
         </div>
       </React.Fragment>
