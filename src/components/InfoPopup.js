31d7de6dd954afc5bf53b1c5f5bb3b76b9871ea5
asalexanderlee <asalexanderlee@davidson.edu>
2018-09-08 11:28:17 -0400

    Add panels, choose random color for panels

--- src/components/InfoPopup.js
+++ src/components/InfoPopup.js
@@ -9,12 +9,35 @@ const Panel = props => {
   );
 };
 export default class InfoPopup extends Component {
+  chooseRandomColor(colors) {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
   render() {
-    const colors = ["#308a8e"];
+    const colors = [
+      "#308a8e",
+      "#db2745",
+      "#43bc0f",
+      "#1e70ce",
+      "#13a07a",
+      "#9b0c96",
+      "#7c44ff",
+      "#d61c00",
+      "#e0cd00",
+      "#001377"
+    ];
     return (
       <div className="popup">
         <div className="row">
-          <Panel color={colors[0]} title={"900 Room"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"After Midnight"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"Live Thursday"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"Duke Family Performance Hall"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"Davis Cafe"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"Career Center"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"Post Office"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"Gym"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"Davidson Outdoors"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"Chaplain's Office"} />
+          <Panel color={this.chooseRandomColor(colors)} title={"Center for Civic Engagement"} />
         </div>
       </div>
     );
