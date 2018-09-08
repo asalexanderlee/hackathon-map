d774eb531bed3d95405d6e08026199a68d41990c
Bannermanj <bannermanj11@yahoo.com>
2018-09-08 11:00:47 -0400

    add map component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+//React Components
+import Map from "./components/Map";
+import Search from "./components/Search";
 import InfoPopup from "./components/InfoPopup.js";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <InfoPopup />
+      <div className="App">
+        <h1 className="App-title">Davidson College Interactive Map</h1>
+        <Map />
+
+        <div>
+          <InfoPopup />
+        </div>
       </div>
     );
   }
